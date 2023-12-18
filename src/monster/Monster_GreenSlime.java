bf4df6c5f64479f98cbc62a8605fb4daf4127667
Rycs123 <riyanda.c.sinambela@gmail.com>
2023-12-18 12:07:07 +0700

    monster

--- src/monster/Monster_GreenSlime.java
+++ src/monster/Monster_GreenSlime.java
@@ -1,7 +1,7 @@
 package monster;
 
 import java.util.Random;
-
+import java.io.IOException;
 import character.Character;
 import main.GamePanel;
 import javax.imageio.ImageIO;
@@ -26,14 +26,19 @@ public class Monster_GreenSlime extends Character {
     }
 
     public void getImage() {
-        up1 = ImageIO.read(getClass().getResourceAsStream("/monster/greenslime_down_1"));
-        up2 = ImageIO.read(getClass().getResourceAsStream("/monster/greenslime_down_2"));
-        down1 = ImageIO.read(getClass().getResourceAsStream("/monster/greenslime_down_1"));
-        down2 = ImageIO.read(getClass().getResourceAsStream("/monster/greenslime_down_2"));
-        left1 = ImageIO.read(getClass().getResourceAsStream("/monster/greenslime_down_1"));
-        left2 = ImageIO.read(getClass().getResourceAsStream("/monster/greenslime_down_2"));
-        right1 = ImageIO.read(getClass().getResourceAsStream("/monster/greenslime_down_1"));
-        right2 = ImageIO.read(getClass().getResourceAsStream("/monster/greenslime_down_2"));
+        try {
+            up1 = ImageIO.read(getClass().getResourceAsStream("/monster/greenslime_down_1"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/monster/greenslime_down_2"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/monster/greenslime_down_1"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/monster/greenslime_down_2"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/monster/greenslime_down_1"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/monster/greenslime_down_2"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/monster/greenslime_down_1"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/monster/greenslime_down_2"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void setAction() {
